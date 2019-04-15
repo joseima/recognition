f7b9995a3755a6ee50a89c12aeba0e8446cf1cc4
joseima <joseima17@gmail.com>
2019-04-15 18:53:50 +0200

    New front

---
+++ precache-manifest.3d3a0bf06758b54ee0989acc9ee59066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d0054b6778d28a0f289",
+    "url": "/static/js/main.cfcdc8fa.chunk.js"
+  },
+  {
+    "revision": "f2c20ddf5bceffddf5f3",
+    "url": "/static/js/2.57c37a30.chunk.js"
+  },
+  {
+    "revision": "5d0054b6778d28a0f289",
+    "url": "/static/css/main.5103021a.chunk.css"
+  },
+  {
+    "revision": "f2c20ddf5bceffddf5f3",
+    "url": "/static/css/2.b82e0898.chunk.css"
+  },
+  {
+    "revision": "e4b44768f2ef167444737e2c7a9236c9",
+    "url": "/index.html"
+  }
+];
